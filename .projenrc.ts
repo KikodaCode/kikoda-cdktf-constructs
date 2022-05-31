5f902159eb5cd565b63faa2d9824c538ea32bf30
Nathan Cazell <nathan.cazell@kikoda.com>
2022-05-31 18:43:12 +0000

    chore(deps): projen upgrade; apply kikoda oss template

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,5 +1,6 @@
-import { cdktf, TextFile } from 'projen';
-import { TrailingComma, ArrowParens, EndOfLine } from 'projen/lib/javascript';
+import { KikodaOpenSourceProject, KikodaStandards } from '@kikoda/projen-templates';
+import { cdktf } from 'projen';
+import { GithubCredentials } from 'projen/lib/github';
 
 const project = new cdktf.ConstructLibraryCdktf({
   name: 'kikoda-cdktf-constructs',
@@ -19,18 +20,7 @@ const project = new cdktf.ConstructLibraryCdktf({
   vscode: true,
   docgen: true,
   prettier: true,
-  prettierOptions: {
-    settings: {
-      printWidth: 100,
-      tabWidth: 2,
-      useTabs: false,
-      semi: true,
-      singleQuote: true,
-      trailingComma: TrailingComma.ALL,
-      arrowParens: ArrowParens.AVOID,
-      endOfLine: EndOfLine.LF,
-    },
-  },
+  prettierOptions: KikodaStandards.PrettierOptions,
   tsconfig: { compilerOptions: { esModuleInterop: true } },
   jestOptions: {
     jestConfig: {
@@ -38,47 +28,16 @@ const project = new cdktf.ConstructLibraryCdktf({
     },
   },
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  // deps: [] /* Runtime dependencies of this module. */,
   // bundledDeps: [],
-  // devDeps: [] /* Build dependencies for this module. */,
+  devDeps: ['@kikoda/projen-templates'] /* Build dependencies for this module. */,
   // gitignore: [],
-  pullRequestTemplateContents: [
-    `## Proposed changes`,
-    '',
-    `_Describe the big picture of your changes here to communicate to the maintainers why we should accept this pull request.If it fixes a bug or resolves a feature request, be sure to link to that issue._`,
-    '',
-    `### Commentary`,
-    '',
-    `_Anything else we should know when reviewing?_`,
-    '',
-    `### Types of Changes`,
-    '',
-    `What types of changes does your code introduce? _Chedk all the boxes that apply_`,
-    '',
-    `- [ ] Bugfix(non - breaking change which fixes an issue)`,
-    `- [ ] New feature or Enhancement(non - breaking change which adds functionality)`,
-    `- [ ] Breaking change(fix or feature that would cause existing functionality to not work as expected)`,
-    `- [ ] Chore(documentation, refactoring, dependency upgrade, etc.)`,
-    '',
-    `### Chores`,
-    '',
-    `_Check all the boxes that apply_`,
-    '',
-    `- [ ] This PR includes breaking changes which are documented in the description and associated commit messages(see the[Contributing Guide](../ CONTRIBUTING.md) for more information on how this should be done)`,
-    `- [ ] This PR impacts documentation, and it has been updated(or an issue has been created and linked)`,
-    `- [ ] This PR's changes are covered by the automated tests`,
-  ],
+  pullRequestTemplate: false,
+  githubOptions: {
+    projenCredentials: GithubCredentials.fromApp(),
+  },
 });
 
-// Generate a NOTICE file for license
-new TextFile(project, 'NOTICE', {
-  marker: false,
-  lines: [
-    'Kikoda CDKTF Constructs Library',
-    'Copyright 2022-2022 Kikoda, LLC',
-    '',
-    'This product includes software developed at Kikoda (https://www.kikoda.com)',
-  ],
-});
+new KikodaOpenSourceProject(project, { title: 'Kikoda CDKTF Constructs Library' });
 
 project.synth();
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# replace this
+[<img src="https://kikoda.com/wp-content/uploads/2019/07/Logo_White_bg.svg" width="300"/>](https://kikoda.com)
+# Kikoda CDKTF Constructs Library
+
+[![NPM](https://img.shields.io/npm/v/@kikoda/cdktf-constructs?color=39a356)](https://www.npmjs.com/package/@kikoda/cdktf-constructs)
+[![License](https://img.shields.io/badge/license-Apache--2.0-blue)](https://github.com/KikodaCode/kikoda-cdktf-constructs.git/blob/main/LICENSE)
+
+Use this Kikoda CDKTF Constructs Library to architect and model modern applications
+
+## Install from NPM:
+```
+yarn add --dev @kikoda/cdktf-constructs
+
+# or
+
+npm install @kikoda/cdktf-constructs --save-dev
+```
+
+## Usage
+<!-- TODO: add Usage & Examples -->
+
+## Opening Issues
+
+If you encounter a bug with this package, we want to hear about it. Before opening a new issue, search the existing issues to avoid duplicates.
+
+When opening an issue, include the Kikoda Construct Library version, Node version, and stack trace if available. In addition, include the steps to reproduce when appropriate.
+
+You can also open an issue for a feature request.
+
+## Contributing
+
+If you find an issue with this package and have a fix, please feel free to open a pull request following the [procedures](CONTRIBUTING.md).
+
+## Testing
+
+If you contribute to this package you can run the tests using `yarn test`.
+
+## License
+
+Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+
+This product includes software developed at Kikoda (https://www.kikoda.com). Copyright 2022 Kikoda, LLC.
